7fdf703f0075703a7f191301471a4cd11f26314a
Dean <imnikz@gmail.com>
2022-08-04 19:52:14 +0700

    Done function add new book

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//All elements and buttons 
+const addBookFormNodeList = document.getElementsByClassName('extra-form');
+const addBtn = document.getElementsByClassName('btn-add')[0];
+const btnSection = document.getElementsByClassName('btn-section')[0];
+const btnAddSection = btnSection.querySelector('.btn-add-book');
+const btnExitSection = btnSection.querySelector('.btn-exit-book');
+const main = document.querySelector('main');
+let myLibrary = [];
+
+
 // The constructor
 function Book(title, author, pages, read) {
     this.title = title; 
@@ -15,50 +25,21 @@ function Book(title, author, pages, read) {
     }   
 }
 
-function addBookToLibrary() {
-    let title = prompt("Enter your book name: ", "Sherlock Holmes");  
-    let author = prompt("Enter the book author: ", "Arthur Conan Doyle");
-    let pages = parseInt(prompt("Enter the book pages: ", "341"));
-    while (isNaN(pages)) {
-        pages = parseInt(prompt("Enter the correct book pages: "));
-    }
-
-    let choice = prompt("Yes if read / No if not. Choose Y/N"); 
-    while (choice !== "Y" && choice !== "N") {
-        choice = prompt("Please choose again! Choose Y/N"); 
-    }
-    const isRead = (choice === "Y") ? true : false; 
-    const newBook = new Book(title, author, pages, isRead); 
-    //Testing console purpose
-    console.log(newBook.info());
-    //3. Add to the 1D array as an objects 
-    myLibrary.push(newBook); 
+function showBookForm() {
+    addBookFormNodeList[0].style.display = 'flex';
 }
 
-let myLibrary = [];
-// addBookToLibrary();
-// addBookToLibrary();
-// addBookToLibrary();
-const sherLock = new Book('Sherlock Holmes', 'Authur Conan Doyle', 341, true);
-const HxH = new Book('Hunter X Hunter', 'Togashi Yoshiro', 6000, false);
-const thinkingFast = new Book('Thinking fast and slow', 'Daniel Kahneman', 781, false);
-const thinkingSlow = new Book('Thinking fast and slow 2', 'Daniel Kahneman', 421, true);
+function exitBookForm() {
+    addBookFormNodeList[0].style.display = 'none';
+}
 
-myLibrary.push(sherLock);
-myLibrary.push(HxH);
-myLibrary.push(thinkingFast);
-myLibrary.push(thinkingSlow);
-
-console.log(myLibrary);
-const main = document.querySelector('main');
-
-for (let i = 0; i < myLibrary.length; i++) {
+function showBooks(book) {
     const card = document.createElement('div');
     card.classList.add('card');
 
     const bookName = document.createElement('div');
     bookName.classList.add('bookName');
-    bookName.textContent = myLibrary[i]['title']; 
+    bookName.textContent = book['title']; 
     card.appendChild(bookName); 
 
     const container = document.createElement('div');
@@ -66,28 +47,58 @@ for (let i = 0; i < myLibrary.length; i++) {
 
     const author = document.createElement('div');
     author.classList.add('author');
-    author.textContent = myLibrary[i]['author'];
+    author.textContent = book['author'];
     container.appendChild(author);
-    
-    // Pages 
+
     const pages = document.createElement('div');
     pages.classList.add('pages');
-    pages.textContent = myLibrary[i]['pages'];
+    pages.textContent = book['pages'];
     container.appendChild(pages);
 
     const isRead = document.createElement('div');
     isRead.classList.add('isRead');
-    if (myLibrary[i]['isRead'] === true) {
+    if (book['isRead'] === true) {
         isRead.textContent = 'Read'
     } else {
         isRead.textContent = 'Not read'
     }
     container.appendChild(isRead);
-
     card.appendChild(container);
 
     //Append card to the main body. 
     main.appendChild(card);
 }
 
+function sendBookForm() {
+    let temp; 
+    // create the new object 
+    if (document.querySelector('select').value === 'read') {
+        temp = true; 
+    } 
+    else {
+        temp = false; 
+    }
+
+    const newBook = new Book(
+        title = document.getElementById('bookName').value,
+        author = document.getElementById('author').value, 
+        pages = document.getElementById('pages').value, 
+        isRead = temp,
+    ); 
+    // put it into the library 
+    myLibrary.push(newBook);
+    // clear the form for the next one 
+    document.querySelector('form').reset();
+    // save it in local storage
+    localStorage.setItem('MyBooksLibrary', JSON.stringify(myLibrary));
+
+    //Show it
+    exitBookForm();
+    showBooks(newBook);
+}
+
+//Buttons to handle 
+addBtn.addEventListener('click', showBookForm);
+btnExitSection.addEventListener('click', exitBookForm);
+btnAddSection.addEventListener('click', sendBookForm); 
 
